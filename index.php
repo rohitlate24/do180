cf0a101bfa37fd498d9cd74befe1b31b417e8f0e
rohitlate24 <123583749+rohitlate24@users.noreply.github.com>
2023-01-25 23:29:55 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page for webhooks 1234 </h1>
+
+<?php
+echo "Hello World! FROM OCT BATCH WEBHOOOKKKKKKK testing webhook learning for KUCL batch";
+ echo "hello from UNNATI 555555.........";
+?>
+
+</body>
+</html> 
