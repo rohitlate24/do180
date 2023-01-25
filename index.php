505e6e4eb7cdc5a293a51bf6fa881bfde5126278
rohitlate24 <123583749+rohitlate24@users.noreply.github.com>
2023-01-25 23:47:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 <body>
 
-<h1>My first PHP page for webhooks 1234 </h1>
+<h1>My first PHP page for webhooks </h1>
 
 <?php
 echo "Hello World! FROM OCT BATCH WEBHOOOKKKKKKK testing webhook learning for KUCL batch";
- echo "hello from UNNATI 555555.........";
+ echo "hello from Rohit.........";
 ?>
 
 </body>
