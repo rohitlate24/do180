2f4f210b3339da843ea17c84aa5b9c94b18806bf
rohitlate24 <123583749+rohitlate24@users.noreply.github.com>
2023-01-25 23:56:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>My first PHP page for webhooks sample </h1>
 
 <?php
-echo "Hello World! FROM OCT BATCH WEBHOOOKKKKKKK testing webhook learning for KUCL batch";
+echo "Hello World! FROM OCT BATCH WEBHOOOKKKKKKK testing webhook learning for KUCL batch"\n;
  echo "hello from Rohit.........";
 ?>
 
